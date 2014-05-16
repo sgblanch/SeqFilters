0b059aadd0872a9fbf6a07e5ad4f595eb2e19faf
Qiong Wang <wangqion@gmail.com>
2014-05-16 14:47:21 -0400

    Updated with more gene options

---
+++ src/edu/msu/cme/pyro/PipelineGene.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.msu.cme.pyro;
+
+/**
+ *
+ * @author fishjord
+ */
+public enum PipelineGene {
+    RRNA_16S_BACTERIA, RRNA_16S_ARCHAEA, RRNA_18S, RRNA_23S, RRNA_28S, OTHER
+}
