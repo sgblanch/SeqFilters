e92e0b8299011325066b65b2f7683d99b0b4a675
Qiong Wang <wangqion@gmail.com>
2014-07-03 10:04:32 -0400

    Added fungalits_warcup gene for Classifier

--- src/edu/msu/cme/pyro/PipelineGene.java
+++ src/edu/msu/cme/pyro/PipelineGene.java
@@ -9,5 +9,5 @@ package edu.msu.cme.pyro;
  * @author fishjord
  */
 public enum PipelineGene {
-    RRNA_16S_BACTERIA, RRNA_16S_ARCHAEA, RRNA_18S, RRNA_23S, RRNA_28S, OTHER
+    RRNA_16S_BACTERIA, RRNA_16S_ARCHAEA, RRNA_18S, RRNA_23S, RRNA_28S, FUNGALITS_WARCUP, OTHER
 }
